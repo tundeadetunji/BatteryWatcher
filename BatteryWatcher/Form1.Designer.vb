ea9c84a514ec72b46a3a838f9115e69f2165b6b1
Adetunji Babatunde <tundeadetunji2017@gmail.com>
2020-03-01 18:42:55 +0100

    Add project files.

---
+++ BatteryWatcher/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+		Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+		Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+		Me.SuspendLayout()
+		'
+		'Timer1
+		'
+		Me.Timer1.Enabled = True
+		Me.Timer1.Interval = 3000
+		'
+		'Timer3
+		'
+		Me.Timer3.Interval = 1000
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(0, 0)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Name = "Form1"
+		Me.Opacity = 0R
+		Me.ShowInTaskbar = False
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Battery Watcher"
+		Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents Timer1 As Timer
+	Friend WithEvents Timer2 As Timer
+	Friend WithEvents Timer3 As Timer
+End Class
---
+++ BatteryWatcher/Form1.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Runtime.InteropServices
+Imports General_Module.FormatWindow
+Imports NModule.NFunctions
+Imports NetModule.NTimer
+Public Class Form1
+	Dim myManagedPower As New ManagedPower()
+	Dim g As New General_Module.FormatWindow
+	Dim n As New NetModule.NTimer
+
+	''' <summary>
+	''' Opens any application stored in Programs.txt in JSON
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+		'open apps if not already open
+		Dim v As String = ReadText(My.Application.Info.DirectoryPath & "\Programs.txt")
+		If v.Length < 1 Then GoTo 3
+		v = v.Replace("\", "\\")
+		Dim s As New List(Of String)
+		Try
+			s.Clear()
+		Catch ex As Exception
+		End Try
+		s = DatabaseToListJSON(v)
+		For i As Integer = 0 To s.Count - 1
+			StartFile(s(i), ProcessWindowStyle.Normal, False, True)
+		Next
+3:
+	End Sub
+
+	Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+		If myManagedPower.ToString().ToLower = "ac" Then
+			StopTimer(Timer2, Timer3)
+		Else
+			StartTimer("Please plug in your machine!", Timer2, Timer3, 3, 0.5, True, False, False, False, False, True)
+		End If
+	End Sub
+End Class
+
+Public Class ManagedPower
+    ' GetSystemPowerStatus() is the only unmanaged API called.
+    Declare Auto Function GetSystemPowerStatus Lib "kernel32.dll" _
+	Alias "GetSystemPowerStatus" (ByRef sps As SystemPowerStatus) As Boolean
+
+	Public Overrides Function ToString() As String
+		Dim text As String = ""
+		Dim sysPowerStatus As SystemPowerStatus
+        ' Get the power status of the system
+        If ManagedPower.GetSystemPowerStatus(sysPowerStatus) Then
+            ' Current power source - AC/DC
+            Dim currentPowerStatus = sysPowerStatus.ACLineStatus
+			text += sysPowerStatus.ACLineStatus.ToString()
+		End If
+		Return text
+	End Function
+
+	<StructLayout(LayoutKind.Sequential)>
+	Public Structure SystemPowerStatus
+		Public ACLineStatus As _ACLineStatus
+		Public BatteryFlag As _BatteryFlag
+		Public BatteryLifePercent As Byte
+		Public Reserved1 As Byte
+		Public BatteryLifeTime As System.UInt32
+		Public BatteryFullLifeTime As System.UInt32
+	End Structure
+
+	Public Enum _ACLineStatus As Byte
+		Battery = 0
+		AC = 1
+		Unknown = 255
+	End Enum
+
+	<Flags()>
+	Public Enum _BatteryFlag As Byte
+		High = 1
+		Low = 2
+		Critical = 4
+		Charging = 8
+		NoSystemBattery = 128
+		Unknown = 255
+	End Enum
+End Class
